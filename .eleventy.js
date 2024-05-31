a2784693ee3d93f39476f0c0054e93fe1755849d
James Barnard <jmbarnard@gmail.com>
2024-05-31 20:04:45 +0100

    init

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,12 @@
 const eleventySass = require("@11tyrocks/eleventy-plugin-sass-lightningcss");
 
 module.exports = function (eleventyConfig) {
+
   eleventyConfig.addPlugin(eleventySass);
+
+  eleventyConfig.addPassthroughCopy("./src/images");
+
+  eleventyConfig.addWatchTarget("./src/images/");
 
   return {
     dir: {
